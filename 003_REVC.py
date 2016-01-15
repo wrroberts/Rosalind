e0742245b1b55a64c8f7f531257ac56091c96e70
Wade Roberts <wade.roberts@wsu.edu>
2016-01-14 19:47:12 -0800

    Reverse Complementing a strand of DNA

---
+++ 003_REVC.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+'''
+A solution to a ROSALIND bioinformatics problem.
+
+Problem Title: Complementing a Strand of DNA
+Rosalind ID: REVC
+ROSALIND #: 003
+URL: http://rosalind.info/problems/revc/
+'''
+
+'''First Solution'''
+
+seq = open('rosalind_rna.txt','r')
+seq = seq.read()
+
+from Bio.Seq import Seq
+seq1 = Seq(seq)
+print seq1.reverse_complement()
+
+'''Second Solution'''
+
+st = open('rosalind_rna.txt','r')
+st = seq.read()
+
+st = st.replace('A','t').replace('T','a').replace('C','g').replace('G','c').upper()[::-1]
+print st
+
