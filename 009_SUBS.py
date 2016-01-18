d519dae2b5128bc611f1cf8c11e6ca62cb78d0b1
Wade Roberts <wade.roberts@wsu.edu>
2016-01-17 16:40:50 -0800

    Finding a Motif in DNA

---
+++ 009_SUBS.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+'''
+A solution to a ROSALIND bioinformatics problem.
+
+Problem Title: Finding a Motif in DNA
+Rosalind ID: SUBS
+Rosalind #: 009
+URL: http://rosalind.info/problems/subs/
+'''
+
+'''First Solution'''
+
+with open('rosalind_subs.txt') as input_data:
+	s,t = input_data.readlines()
+	s = s.rstrip()
+	t = t.rstrip()
+
+locations = []
+
+for i in range(0, len(s)-len(t)+1):
+	if s[i:i+len(t)]==t:
+		locations.append(str(i+1))
+
+print ' '.join(locations)
